3ba70a85aa63d4bb2b89777cc89c7ebd04cec16f
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-14 13:12:56 -0300

    feat: Atribuindo os components do index.js para o App.jsx

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
 
 export default function App(props){
-    
+    return(
+        <div id='app'>
+            <h1>Fundamentos React</h1>
+            <Primeiro></Primeiro>
+            <ComParametro titulo="Titulo Principal" subtitulo="Titulo Secundario"></ComParametro>
+            <Fragmento></Fragmento>
+        </div>
+    );
 }
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,16 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import React from "react";
 import './index.css';
-import Primeiro from './components/basicos/Primeiro';
-import ComParametro from './components/basicos/ComParametro';
-import Fragmento from './components/basicos/Fragmento';
+import App from './App';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
-    <div id='app'>
-        <Primeiro></Primeiro>
-        <ComParametro titulo="Titulo Principal" subtitulo="Titulo Secundario"></ComParametro>
-        <Fragmento></Fragmento>
-    </div>
+    <App></App>
 )
