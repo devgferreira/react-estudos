c975f3d9a396b5a3ea1f94bcfa72c34d51eb6896
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-14 12:20:52 -0300

    feat: importando o arquivo Fragmento.jsx - index.js

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,11 +3,12 @@ import React from "react";
 import './index.css';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
-    <div>
+    <div id='app'>
         <Primeiro></Primeiro>
         <ComParametro titulo="Titulo Principal" subtitulo="Titulo Secundario"></ComParametro>
     </div>
