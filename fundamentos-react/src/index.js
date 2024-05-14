926eb0c3e623552b80f23d3053e8752ebe84faf0
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-14 11:26:57 -0300

    feat: criando o primeiro render  - index.js

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(
+    <div>
+        <strong>
+            Hello World
+        </strong>
+    </div>
+    )
