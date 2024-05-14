256ee0b31903af005065c84ecd0282465e100e45
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-14 15:15:53 -0300

    feat: colocando os componentes dentro do Card - App.jsx

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -9,11 +9,21 @@ export default function App(props) {
     return (
         <div id='app'>
             <h1>Fundamentos React</h1>
-            <Card titulo="Exemplo"></Card>
-            <Primeiro></Primeiro>
-            <ComParametro titulo="Titulo Principal" subtitulo="Titulo Secundario"></ComParametro>
-            <Fragmento></Fragmento>
-            <NumeroAleatorio max={50} min={10}></NumeroAleatorio>
+            <Card titulo="Primeiro #1">
+                <Primeiro></Primeiro>
+
+            </Card>
+            <Card titulo="Com Parâmetro #2">
+                <ComParametro titulo="Titulo Principal" subtitulo="Titulo Secundario"></ComParametro>
+
+            </Card>
+            <Card titulo="Fragmento #3">
+                <Fragmento></Fragmento>
+            </Card>
+            <Card titulo="Desafio Aleatório #4">
+                <NumeroAleatorio max={50} min={10}></NumeroAleatorio>
+            </Card>
+            
         </div>
     );
 }
