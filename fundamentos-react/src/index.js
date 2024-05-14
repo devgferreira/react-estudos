ac0b0acc3c265de736b307519741cfded97b0c09
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-14 11:50:28 -0300

    feat: importando o arquivo Primeiro.js - index.js

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,12 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import './index.css'
+import Primeiro from './components/basicos/Primeiro'
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
     <div>
         <strong>
-            Hello World
+            <Primeiro></Primeiro>
         </strong>
     </div>
 )
