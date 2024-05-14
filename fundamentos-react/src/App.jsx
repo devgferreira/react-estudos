80f17c5281ba09b7595e3170e6dabd8a97ae1437
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-14 14:21:19 -0300

    feat: criando o css para Card, Tile e Content - Card.css

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -3,11 +3,13 @@ import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import NumeroAleatorio from "./components/basicos/NumeroAleatorio";
+import Card from "./components/layout/Card";
 
 export default function App(props) {
     return (
         <div id='app'>
             <h1>Fundamentos React</h1>
+            <Card titulo="Exemplo"></Card>
             <Primeiro></Primeiro>
             <ComParametro titulo="Titulo Principal" subtitulo="Titulo Secundario"></ComParametro>
             <Fragmento></Fragmento>
