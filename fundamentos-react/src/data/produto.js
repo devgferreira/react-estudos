c20d46dcdbad0deb8e835d967f848331cc70a6c7
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-15 15:28:31 -0300

    feat: criando um array para uma lista de produos - produto,.js

---
+++ fundamentos-react/src/data/produto.js
@@ -0,0 +1,8 @@
+export default [
+    { id: 1, nome: 'Batata', preco: 7.5 },
+    { id: 2, nome: 'Cenoura', preco: 8.2 },
+    { id: 3, nome: 'Caderno', preco: 6.4 },
+    { id: 4, nome: 'Lapis', preco: 9.3 },
+    { id: 5, nome: 'Caena', preco: 7.8 },
+    { id: 6, nome: 'Camiseta', preco: 8.1 }
+]
