8d9dd18aee48ce8d650f0f0e42a9a91fab650d0f
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-14 22:09:19 -0300

    feat: criando a function: ListaAlunos - ListaAlunos.jsx

---
+++ fundamentos-react/src/components/repeticao/ListaAlunos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ListaAlunos(props){
+    return(
+        <div>
+            <ul>
+                <li>Gabriel - 10</li>
+                <li>Lucas - 8.2</li>
+                <li>João - 5.7</li>
+            </ul>
+        </div>
+    );
+}
