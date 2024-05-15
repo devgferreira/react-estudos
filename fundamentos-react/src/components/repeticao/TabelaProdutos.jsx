89ae1bbec7c07a20345d347ea4e19a6084c29256
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-15 15:48:45 -0300

    feat: trazendo os dados do arquivo produtos.js e carregando os dados na tabela

--- fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
+++ fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
@@ -1,36 +1,33 @@
 import React from "react";
-import produto from "../../data/produto";
+import produtos from "../../data/produtos";
+import "./TabelaProdutos.css"
 
 export default function TabelaProdutos(props) {
+
+    const row = produtos.map((produto) => {
+        return (
+            <tr>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>R$ {produto.preco}</td>
+            </tr>
+        );
+    })
+
     return (
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="container mt-3">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {row}
+                </tbody>
+            </table>
+        </div>
     );
 }
