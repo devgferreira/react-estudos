fc9bc651be0c7d76cfdf530a13bb9abe968a1d40
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-14 22:19:25 -0300

    feat: exportando um array de alunos - alunos.js

---
+++ fundamentos-react/src/data/alunos.js
@@ -0,0 +1,8 @@
+export default [
+    { id: 1, nome: 'Ana', nota: 7.5 },
+    { id: 2, nome: 'Bia', nota: 8.2 },
+    { id: 3, nome: 'Carlos', nota: 6.4 },
+    { id: 4, nome: 'Daniel', nota: 9.3 },
+    { id: 5, nome: 'Eduardo', nota: 7.8 },
+    { id: 6, nome: 'Fernanda', nota: 8.1 }
+]
