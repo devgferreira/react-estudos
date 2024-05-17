f85994e64b95339f78a0a5b01549f8196815cfc6
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-17 13:30:15 -0300

    feat: adicionando o useState para setar a mudança de dados - IndiretaPai.jsx

--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 export default function IndiretaPai(props) {
-    let nome = '?'
-    let idade = 0
-    let nerd =  false
+    let [nome, setNome] = useState('?')
+    let [idade, setIdade] = useState(0)
+    let [nerd, setNerd] = useState(false)
     function fornecerInformacoes(Nome, Idade, Nerd) {
-        nome = Nome;
-        idade = Idade;
-        nerd = Nerd
+        setNome(Nome)
+        setIdade(Idade)
+        setNerd(Nerd)
     }
     return (
         <div>
             <div>
-                <span>{nome}</span>
-                <span>{idade}</span>
-                <span>{nerd ? "Verdadeiro" : "Falso"}</span>
+                <span>{nome} </span>
+                <span>{idade} </span>
+                <span>{nerd ? "Verdadeiro" : "Falso"}!</span>
             </div>
             <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
         </div>
