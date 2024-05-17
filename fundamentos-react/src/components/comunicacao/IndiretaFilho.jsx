cdcece978bda453f996d144be5524800236782e0
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-17 12:13:40 -0300

    feat: criando a function IndiretaFilho - IndiretaFilho.jsx

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-export default function IndiretaFilho(props){
-    return(
+export default function IndiretaFilho(props) {
+    return (
         <div>
+            <div>
+                <div>Filho</div>
+                <button onClick={{
+                    function(e) {
+                        props.quandoClicar("Gabriel", 53, true)
+                    }
+                }}></button>
+            </div>
         </div>
     );
 }
