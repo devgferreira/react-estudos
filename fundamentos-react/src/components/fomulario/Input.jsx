a198e23a8c0bbd1525b423aae620c130d35829ee
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-18 13:21:13 -0300

    feat: criando a function Input - Input.jsx

--- fundamentos-react/src/components/fomulario/Input.jsx
+++ fundamentos-react/src/components/fomulario/Input.jsx
@@ -1,7 +1,21 @@
-
+import { useState } from "react";
+import "./Input.css"
 
 export default function Input(props){
+    const [valor, setValor] = useState("Inicial");
+
+    function quandoMudar(e){
+
+        setValor(e.target.value)
+    }
+
     return(
-        <div></div>
+        <div className="Input">
+            <div>
+                <input type="text" value={valor} onChange={quandoMudar} />
+                <input type="text" value={valor} readOnly />
+                <input type="text" value={undefined} />
+            </div>
+        </div>
     );
 }
