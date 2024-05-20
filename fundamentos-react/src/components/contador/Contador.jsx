e6a23caac57d8f874212298dbb31fc09ca47f7d0
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-20 15:04:36 -0300

    feat: criando os métodos: inc, dec e setPasso - Contador.jsx

--- fundamentos-react/src/components/contador/Contador.jsx
+++ fundamentos-react/src/components/contador/Contador.jsx
@@ -1,11 +1,41 @@
 import React from "react"
+import './Contador.css'
 
-export default class Contador extends React.Component{
+export default class Contador extends React.Component {
 
-    render(){
-        return(
-            <div>
+    state = {
+        numero: this.props.numeroInicial || 0,
+        passo: this.props.passoInicial || 5
+    }
+    inc = () => {
+        this.setState({
+            numero: this.state.numero + this.state.passo
+        })
+    }
+
+    dec = () => {
+        this.setState({
+            numero: this.state.numero - this.state.passo
+        })
+    }
+
+    setPasso = (e) =>{
+        this.setState({
+            passo: +e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="Contador">
                 Contador
+                <h3>{this.state.numero}</h3>
+                <div>
+                    <label htmlFor="passoInput">Passo: </label>
+                    <input id="passoInput" type="number" value={this.state.passo} onChange={this.setPasso} />
+                </div>
+                <button onClick={this.inc}>+</button>
+                <button onClick={this.dec}>-</button>
             </div>
         )
     }
