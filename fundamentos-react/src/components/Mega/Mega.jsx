e21bfad89143e67859bd31c09c6714f7c81592ba
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-21 14:50:20 -0300

    feat: criando a function para gerar numéros aleatorio para a megasena - Mega.jsx

--- fundamentos-react/src/components/Mega/Mega.jsx
+++ fundamentos-react/src/components/Mega/Mega.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 
-export default function Mega(props){
+export default function Mega(props) {
+    const [numeroInicial, setNumeroInicial] = useState(props.numeroInicial)
+    const [numerosSorteados, setNumerosSorteados] = useState([]);
+    
+    function quandoMudar(e){
+
+        setNumeroInicial(e.target.value)
+    }
+
+    function gerarNumerosAleatorios() {
+        const megaNumero = new Set();
+
+        while (megaNumero.size < numeroInicial) {
+            const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+            megaNumero.add(numeroAleatorio);
+        }
+
+        const numerosOrdenados = Array.from(megaNumero).sort((a, b) => a - b);
+        setNumerosSorteados(numerosOrdenados);
+    }
+
     return (
         <div>
+            <div>
+                <label htmlFor="megasenaNumeroInput">Número para gerar da Megasena:</label>
+                <input id="megasenaNumeroInput" type="number" value={numeroInicial} onChange={quandoMudar} />
+            </div>
             
+            <div>
+                <button onClick={gerarNumerosAleatorios}>Gerar</button>
+            </div>
+            <label htmlFor="">Números sorteados: {numerosSorteados.join(', ')}</label>
         </div>
     );
 }
