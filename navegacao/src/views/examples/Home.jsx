638d046b9acb7eb5b94f5875f1681fb7c3358cc5
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-27 12:59:39 -0300

    feat: mudando a esturura para array function sem return

--- navegacao/src/views/examples/Home.jsx
+++ navegacao/src/views/examples/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Home = props => {
+const Home = props => (
     <div >
         <h1>Componente Home</h1>
         <p>Bem vindo</p>
     </div>
-}
+)
 
 export default Home
 
