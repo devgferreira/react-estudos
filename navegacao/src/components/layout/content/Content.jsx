3423f382c43a383a06c4c16535b3ba08994a2c14
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-27 12:58:18 -0300

    feat: importando o Routes e Route para navegação entre os componentes - Content.jsx

--- navegacao/src/components/layout/content/Content.jsx
+++ navegacao/src/components/layout/content/Content.jsx
@@ -1,11 +1,17 @@
 import './Content.css'
 import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import About from '../../../views/examples/About';
+import Home from '../../../views/examples/Home';
 
-const Content = props => {
+const Content = props => (
     <main className="Content">
-        Componente Content
+        <Routes>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/' element={<Home/>}/>
+        </Routes>
     </main>
-}
+)
 
 export default Content
 
