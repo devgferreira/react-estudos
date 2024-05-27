e0c49b9fac8b0595319d1cdfd131c0282047a88b
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-27 13:58:49 -0300

    feat: adicionando mais uma roa - Content.jsx

--- navegacao/src/components/layout/content/Content.jsx
+++ navegacao/src/components/layout/content/Content.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import About from '../../../views/examples/About';
 import Home from '../../../views/examples/Home';
+import Param from '../../../views/examples/Param';
 
 const Content = props => (
     <main className="Content">
         <Routes>
             <Route path='/about' element={<About/>}/>
             <Route path='/' element={<Home/>}/>
+            <Route path='/param/:id' element={<Param/>}/>
         </Routes>
     </main>
 )
