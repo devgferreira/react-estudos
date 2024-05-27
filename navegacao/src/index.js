afbd8dc029c6a6f0ebef2ef6431c9128edef4045
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-05-27 12:32:21 -0300

    feat: importando o css  - index.js

--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
+import './index.css'
 import ReactDOM from 'react-dom/client';
 import App from './views/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App></App>
-  </React.StrictMode>
 );
 
-reportWebVitals();
